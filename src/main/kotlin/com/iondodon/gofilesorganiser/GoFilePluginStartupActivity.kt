83f42ef1ca3fd16d13307f615939fe8e4ddb9744
Ion Dodon <iondodon2@gmail.com>
2023-09-29 16:54:22 +0300

    Open parent .go file on single click

--- src/main/kotlin/com/iondodon/gofilesorganiser/GoFilePluginStartupActivity.kt
+++ src/main/kotlin/com/iondodon/gofilesorganiser/GoFilePluginStartupActivity.kt
@@ -5,7 +5,11 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
+import java.awt.event.MouseMotionAdapter
+import javax.swing.event.TreeExpansionEvent
+import javax.swing.event.TreeWillExpandListener
 import javax.swing.tree.DefaultMutableTreeNode
+import javax.swing.tree.ExpandVetoException
 import javax.swing.tree.TreePath
 
 class GoFilePluginStartupActivity : StartupActivity {
@@ -16,16 +20,17 @@ class GoFilePluginStartupActivity : StartupActivity {
 
         tree.addMouseListener(object : MouseAdapter() {
             override fun mouseClicked(e: MouseEvent) {
-                if (e.clickCount == 2) {  // Detecting double-click
-                    val path: TreePath = tree.getPathForLocation(e.x, e.y) ?: return
-                    val lastPathComponent = path.lastPathComponent as? DefaultMutableTreeNode ?: return
-                    val userObject = lastPathComponent.userObject
-                    if (userObject is GoFileGroupNode) {
+                val path: TreePath = tree.getPathForLocation(e.x, e.y) ?: return
+                val lastPathComponent = path.lastPathComponent as? DefaultMutableTreeNode ?: return
+                val userObject = lastPathComponent.userObject
+                if (userObject is GoFileGroupNode) {
+                    if (e.clickCount == 1) {
                         userObject.navigate(true)
-                        e.consume()  // Consume the event to prevent default double-click behavior
+                        e.consume()
                     }
                 }
             }
         })
+
     }
 }
